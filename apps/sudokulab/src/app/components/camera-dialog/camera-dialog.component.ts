02af121e0728f2abce4088ea1a309673c851a7a3
olmil <leo.olmi@gmail.com>
2021-10-14 12:49:15 +0200

    choose camera

--- apps/sudokulab/src/app/components/camera-dialog/camera-dialog.component.ts
+++ apps/sudokulab/src/app/components/camera-dialog/camera-dialog.component.ts
@@ -10,11 +10,23 @@ import {
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CameraDialogOptions, CameraDialogResult, MessageType, SudokuFacade, SudokuMessage } from '@sudokulab/model';
 import { BehaviorSubject } from 'rxjs';
+import { cloneDeep as _clone } from 'lodash';
+
+const CAMERA_ICON = {
+  front: 'camera_front',
+  rear: 'camera_rear'
+}
+
+interface DeviceInfo {
+  name: string;
+  id: string;
+  _dev: any;
+}
 
 const CAMERA_CONSTRAINTS: any = {
-  facingMode: {
-    exact: 'environment'
-  },
+  // facingMode: {
+  //   exact: 'environment'
+  // },
   video: {
     width: {
       max: 720,
@@ -39,6 +51,11 @@ export class CameraDialogComponent implements AfterViewInit, OnDestroy {
   isRunning$: BehaviorSubject<boolean>;
   message$: BehaviorSubject<string>;
   error$: BehaviorSubject<boolean>;
+  devices$: BehaviorSubject<DeviceInfo[]>;
+
+  isDoubleCam$: BehaviorSubject<boolean>;
+  camIcon$: BehaviorSubject<string>;
+
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: CameraDialogOptions,
               private _sudoku: SudokuFacade,
@@ -47,6 +64,9 @@ export class CameraDialogComponent implements AfterViewInit, OnDestroy {
     this.isRunning$ = new BehaviorSubject<boolean>(false);
     this.error$ = new BehaviorSubject<boolean>(false);
     this.message$ = new BehaviorSubject<string>('initializing...');
+    this.isDoubleCam$ = new BehaviorSubject<boolean>(false);
+    this.camIcon$ = new BehaviorSubject<string>('');
+    this.devices$ = new BehaviorSubject<DeviceInfo[]>([]);
 
     this.message$.subscribe(message => _sudoku.raiseMessage(new SudokuMessage({ message })));
   }
@@ -62,7 +82,41 @@ export class CameraDialogComponent implements AfterViewInit, OnDestroy {
       const mediaStream = await navigator.mediaDevices.getUserMedia(CAMERA_CONSTRAINTS);
       mediaStream.getTracks().forEach(track => track.stop());
     } catch (err) {
-      console.error('Error while stopping media stream video', err);
+      this._error(err, 'Error while stopping media stream video');
+    }
+  }
+
+  async setMedia(dev: DeviceInfo) {
+
+    const constraints: any = _clone(CAMERA_CONSTRAINTS);
+    constraints.video.deviceId = { exact: dev.id };
+
+    const canvas = (<HTMLCanvasElement>this.canvas?.nativeElement);
+    const webcam = (<HTMLVideoElement>this.webcam?.nativeElement);
+    const ctx: CanvasDrawImage = <CanvasDrawImage>canvas.getContext("2d");
+
+    try {
+      await this._close();
+      this.isRunning$.next(false);
+      const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+
+      webcam.srcObject = mediaStream;
+      webcam.play();
+
+      this.isRunning$.next(true);
+
+      const runProcessing = async () => {
+        if (!this.isRunning$.getValue()) {
+          mediaStream.getTracks().forEach(track => track.stop());
+          return
+        }
+        ctx.drawImage(webcam, 0, 0, constraints.video.width.max, constraints.video.height.max);
+        requestAnimationFrame(runProcessing);
+      }
+
+      runProcessing();
+    } catch (err) {
+      this._error(err, 'Errors while initializing camera');
     }
   }
 
@@ -75,26 +129,16 @@ export class CameraDialogComponent implements AfterViewInit, OnDestroy {
     canvas.setAttribute('height', `${CAMERA_CONSTRAINTS.video.height.max}px`)
     webcam.setAttribute('width', `${CAMERA_CONSTRAINTS.video.width.max}px`)
     webcam.setAttribute('height', `${CAMERA_CONSTRAINTS.video.height.max}px`)
-    const ctx: CanvasDrawImage = <CanvasDrawImage>canvas.getContext("2d");
 
     try {
-      CAMERA_CONSTRAINTS.facingMode = { exact: 'environment' };
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      this.devices$.next(devices.filter(dev => dev.kind === 'videoinput').map((dev, i) => ({
+        name: dev.label || `Camera ${(i+1)}`,
+        id: dev.deviceId,
+        _dev: dev
+      })));
 
-      const mediaStream = await navigator.mediaDevices.getUserMedia(CAMERA_CONSTRAINTS);
-      webcam.srcObject = mediaStream;
-      webcam.play();
-      this.isRunning$.next(true);
-
-      const runProcessing = async () => {
-        if (!this.isRunning$.getValue()) {
-          mediaStream.getTracks().forEach(track => track.stop());
-          return
-        }
-        ctx.drawImage(webcam, 0, 0, CAMERA_CONSTRAINTS.video.width.max, CAMERA_CONSTRAINTS.video.height.max);
-        requestAnimationFrame(runProcessing);
-      }
-
-      runProcessing();
+      this.setMedia(this.devices$.getValue()[0]);
     } catch (err) {
       this._error(err, 'Errors while initializing camera');
     }
@@ -130,5 +174,9 @@ export class CameraDialogComponent implements AfterViewInit, OnDestroy {
 
   ngOnDestroy() {
     this._close();
+  }
+
+  toggleCam() {
+
   }
 }
