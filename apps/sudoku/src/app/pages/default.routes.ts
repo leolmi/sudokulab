4d0c3d97ecacfb048d423ca736e99b64fd5a7a86
Leonardo <leo.olmi@gmail.com>
2025-04-24 18:46:04 +0200

    Update default.routes.ts

--- apps/sudoku/src/app/pages/default.routes.ts
+++ apps/sudoku/src/app/pages/default.routes.ts
@@ -1,10 +1,16 @@
 import { Route } from '@angular/router';
+import { PLAYER_PAGE_ROUTE } from './player/player.manifest';
 
-export const FALLBACK_PAGE_ROUTE = 'player';
+export const FALLBACK_PAGE_ROUTE = PLAYER_PAGE_ROUTE;
 
 export const defaultRoutes: Route[] = [
   {
     path: '',
+    redirectTo: `/${FALLBACK_PAGE_ROUTE}`,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
     redirectTo: `/${FALLBACK_PAGE_ROUTE}`,
     pathMatch: 'full'
   },
