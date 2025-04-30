0c8af57543dd8396a47d57bdaf0e39df58dbde96
Leonardo <leo.olmi@gmail.com>
2025-04-30 20:32:33 +0200

    Update XYWings.algorithm.ts

--- libs/algorithms/src/lib/catalog/XYWings.algorithm.ts
+++ libs/algorithms/src/lib/catalog/XYWings.algorithm.ts
@@ -1,10 +1,23 @@
-import { Algorithm, AlgorithmOptions, AlgorithmResult, AlgorithmType, SudokuCell } from '@olmi/model';
-import { applyAlgorithm } from '../algorithms.common';
+import {
+  Algorithm,
+  AlgorithmOptions,
+  AlgorithmResult,
+  AlgorithmType, canSee,
+  getByVisibles,
+  getCoord,
+  SudokuCell
+} from '@olmi/model';
+import { applyAlgorithm, getHighlights, getSingleResultLine, onRemoved, registerAlgorithm } from '../algorithms.common';
+import { intersection as _intersection } from 'lodash';
 
 export const XYWINGS_ALGORITHM = 'XYWings';
 
 /**
  * ALGORITMO XY-Wings
+ *
+ * Quando due celle dette ali entrambe con due valori possibili condividono tali valori alternativamente
+ * con la terza cella detta pivot, il terzo valore condiviso solo dalle ali può essere escluso dalle celle
+ * vuote che "vedono le ali
  *
  * fattore: +190
  */
@@ -15,7 +28,9 @@ export class XYWingsAlgorithm extends Algorithm {
   name = 'XY-Wings';
   icon = 'widgets';
   type = AlgorithmType.support;
-  title = 'XY-Wings';
+  title = `Quando due celle dette ali entrambe con due valori possibili condividono tali valori alternativamente
+  con la terza cella detta pivot, il terzo valore condiviso solo dalle ali può essere escluso dalle celle
+  vuote che "vedono le ali"`;
   description = `Come altri algoritmi non risolve un valore specifico ma contribuisce nell'escludere valori possibili dalle celle interessate`;
   options = <AlgorithmOptions>{
     checkAvailableOnStep: true
@@ -23,99 +38,53 @@ export class XYWingsAlgorithm extends Algorithm {
 
   apply = (cells: SudokuCell[]): AlgorithmResult =>
     applyAlgorithm(this, cells, (res) => {
-
-      // TODO...
-
-
+      return !!cells.find(pivot => {
+        if (pivot.available.length === 2) {
+          return !!cells.find(cw1 => {
+            const w1v = _intersection(cw1.available, pivot.available);
+            // la prima cella wing deve avere uno dei suoi valori possibili
+            // in comune con uno del pivot e "vederlo"
+            if (cw1.id !== pivot.id && canSee(pivot, cw1) && cw1.available.length===2 && w1v.length === 1) {
+              // pivot + cw1
+              return !!cells.find(cw2 => {
+                const w2v = _intersection(cw2.available, pivot.available);
+                const w12v = _intersection(cw1.available, cw2.available);
+                // la seconda cella wing deve "vedere" il pivot, avere anch'essa 2 soli valori possibili,
+                // uno in comune col pivot e l'altro in comune con la prima cella wing
+                if (cw2.id !== pivot.id && cw2.id !== cw1.id &&
+                  canSee(pivot, cw2) &&
+                  cw2.available.length === 2 &&
+                  w2v.length === 1 && w1v[0] !== w2v[0] &&
+                  w12v.length === 1) {
+                  // pivot [A,B] + cw1 [A,X] + cw2 [B,X]
+                  // adesso nelle celle che "vedono cw1 e cw2" diverse dal pivot è possibile escludere
+                  // il valore X comune alle wing ma non presente nel pivot
+                  const v = w12v[0];
+                  const cids = [pivot.id, cw1.id, cw2.id];
+                  return !!getByVisibles(cells, [cw1.id, cw2.id]).find(tc => {
+                    // la cella trovata deve essere diversa dal pivot e dalle ali
+                    if (!cids.includes(tc.id)) {
+                      onRemoved(tc, v, (removed) => {
+                        res.applied = true;
+                        res.highlights = getHighlights(tc, cids);
+                        res.descLines = getSingleResultLine(tc,
+                          `Found ${this.name} on cells ${cids.map(cid => getCoord(cid)).join(',')}, so on cell ${tc.coord} the possible values [${removed.join(',')}] have been removed`);
+                      });
+                    }
+                    return res.applied;
+                  });
+                }
+                return res.applied;
+              });
+            }
+            return res.applied;
+          });
+        }
+        return res.applied;
+      })
     })
 }
 
 
-// import {Algorithm} from "../Algorithm";
-// import {AlgorithmType} from "../enums";
-// import {PlaySudoku} from "../PlaySudoku";
-// import {AlgorithmResult, AlgorithmResultLine} from "../AlgorithmResult";
-// import {
-//   applyAlgorithm,
-//   findCommonAffectedCells,
-//   hasNAvailable,
-//   notifyApplied,
-//   NotifyAppliedArgs,
-//   onCells
-// } from "./algorithms.common";
-// import {PlaySudokuCell} from "../PlaySudokuCell";
-// import {getUserCoord} from "../../sudoku.helper";
-// import {remove as _remove} from 'lodash';
-//
-// export const XYWINGS_ALGORITHM = 'XYWings';
-//
-// /**
-//  * ALGORITMO
-//  * XY-Wings
-//  *
-//  *
-//  *
-//  * fattore: +190
-//  */
-// export class XYWingsAlgorithm extends Algorithm {
-//   id = XYWINGS_ALGORITHM;
-//   // xy-wings è più difficile da vedere e usare se sono molti i numeri mancanti
-//   factor = '+190';
-//   name = 'XY-Wings';
-//   icon = 'widgets';
-//   type = AlgorithmType.support;
-//   title = '...';
-//   description = `Come altri algoritmi non risolve un valore specifico ma contribuisce nell'escludere valori possibili dalle celle interessate`;
-//   apply = (sdk: PlaySudoku): AlgorithmResult => {
-//     return applyAlgorithm(this, sdk, (o) => {
-//       onCells(sdk, (c1) => {
-//         if (c1 && hasNAvailable(c1)) {
-//           const [x, y] = c1.availables;
-//           // Trova due celle "wings" collegate al pivot
-//           const wings = findXYWingWings(sdk, c1);
-//           if (wings.length > 1) {
-//             for (let i = 0; i < wings.length - 1; i++) {
-//               for (let j = i + 1; j < wings.length; j++) {
-//                 const wing1 = wings[i];
-//                 const wing2 = wings[j];
-//                 if (
-//                   wing1.availables.length === 2 &&
-//                   wing2.availables.length === 2 &&
-//                   wing1.availables.includes(x) &&
-//                   wing1.availables.includes(y) &&
-//                   wing2.availables.includes(x) &&
-//                   wing2.availables.includes(y)) {
-//                   // Candidato comune
-//                   const shared = wing1.availables.find(c => wing2.availables.includes(c));
-//                   if (shared) {
-//                     const afected = findCommonAffectedCells(sdk, wing1, wing2);
-//                     afected.forEach(ac => {
-//                       const removed = _remove(ac.availables || [], v => v === shared);
-//                       const args = <NotifyAppliedArgs>{
-//                         cid: ac.id,
-//                         cids: [c1.id, wing1.id, wing2.id],
-//                         removed
-//                       };
-//                       notifyApplied(this, o, args);
-//                     });
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       });
-//     });
-//   }
-// }
-//
-// const findXYWingWings = (sdk: PlaySudoku, pivot: PlaySudokuCell): PlaySudokuCell[] => {
-//   const wings: PlaySudokuCell[] = [];
-//   onCells(sdk, (c) => {
-//     // La cella deve avere esattamente 2 candidati e condividere uno dei candidati con il pivot
-//     if (c && hasNAvailable(c) && c.availables.some(v => pivot.availables.includes(v))) {
-//       wings.push(c);
-//     }
-//   }, [pivot]);
-//   return wings;
-// }
+// registra l'algoritmo
+registerAlgorithm(new XYWingsAlgorithm());
